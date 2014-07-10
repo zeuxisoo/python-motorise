4e1b42a0207dec5e29af6ccd58253b79086b8766
Zeuxis Lo <seekstudio@gmail.com>
2014-07-11 00:55:24 +0800

    update setup.py

--- setup.py
+++ setup.py
@@ -19,10 +19,21 @@ setup(
     license='BSD',
     author=author,
     author_email=author_email,
-    description='A toy for automating interaction with websites',
+    description='A toy for automating interaction with websites like mechanize',
     packages=find_packages(exclude=['tests', 'tests.*']),
     include_package_data=True,
     zip_safe=False,
     platforms='any',
     install_requires=requires,
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Topic :: Internet",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Internet :: WWW/HTTP :: Browsers"
+    ],
 )
